92de223c3dd878a8ae6890f658679bf59ec81e21
NakamuraKazuha <82699183+NakamuraKazuha@users.noreply.github.com>
2025-06-02 14:17:21 +0800

    cards and deck logic

--- CardDatabase.gd
+++ CardDatabase.gd
@@ -1,4 +1,63 @@
-const  CARDS = {
-	"diamond_Alas": [],
-	"diamond2" : []
+extends Node
+
+const CARDS = {
+	# Diamonds
+	"A_DIAMOND": { "value": 11, "alt_value": 1 },
+	"2_DIAMOND": { "value": 2 },
+	"3_DIAMOND": { "value": 3 },
+	"4_DIAMOND": { "value": 4 },
+	"5_DIAMOND": { "value": 5 },
+	"6_DIAMOND": { "value": 6 },
+	"7_DIAMOND": { "value": 7 },
+	"8_DIAMOND": { "value": 8 },
+	"9_DIAMOND": { "value": 9 },
+	"10_DIAMOND": { "value": 10 },
+	"J_DIAMOND": { "value": 10 },
+	"Q_DIAMOND": { "value": 10 },
+	"K_DIAMOND": { "value": 10 },
+
+	# Clubs
+	"A_CLUB": { "value": 11, "alt_value": 1},
+	"2_CLUB": { "value": 2 },
+	"3_CLUB": { "value": 3 },
+	"4_CLUB": { "value": 4 },
+	"5_CLUB": { "value": 5 },
+	"6_CLUB": { "value": 6 },
+	"7_CLUB": { "value": 7 },
+	"8_CLUB": { "value": 8 },
+	"9_CLUB": { "value": 9 },
+	"10_CLUB": { "value": 10 },
+	"J_CLUB": { "value": 10 },
+	"Q_CLUB": { "value": 10 },
+	"K_CLUB": { "value": 10 },
+
+	# Hearts
+	"A_HEART": { "value": 11, "alt_value": 1 },
+	"2_HEART": { "value": 2 },
+	"3_HEART": { "value": 3 },
+	"4_HEART": { "value": 4 },
+	"5_HEART": { "value": 5 },
+	"6_HEART": { "value": 6 },
+	"7_HEART": { "value": 7 },
+	"8_HEART": { "value": 8 },
+	"9_HEART": { "value": 9 },
+	"10_HEART": { "value": 10 },
+	"J_HEART": { "value": 10 },
+	"Q_HEART": { "value": 10 },
+	"K_HEART": { "value": 10 },
+
+	# Spades
+	"A_SPADE": { "value": 11, "alt_value": 1 },
+	"2_SPADE": { "value": 2 },
+	"3_SPADE": { "value": 3 },
+	"4_SPADE": { "value": 4 },
+	"5_SPADE": { "value": 5 },
+	"6_SPADE": { "value": 6 },
+	"7_SPADE": { "value": 7 },
+	"8_SPADE": { "value": 8 },
+	"9_SPADE": { "value": 9 },
+	"10_SPADE": { "value": 10 },
+	"J_SPADE": { "value": 10 },
+	"Q_SPADE": { "value": 10 },
+	"K_SPADE": { "value": 10 }
 }
--- Deck.gd
+++ Deck.gd
@@ -1,31 +1,34 @@
 extends Node2D
 
-var player_deck = ["diamond_Alas", "diamond_2"]
 const CARD_SCENE_PATH = "res://Scenes/Card.tscn"
+const CARD_DATABASE = preload("res://CardDatabase.gd")
+
+var player_deck = []  # Will be filled from CardDatabase
 
 var card_database_reference
 
 func _ready() -> void:
-	pass
 	#print($Area2D.collision_mask)
+	player_deck = CARD_DATABASE.CARDS.keys()
 	player_deck.shuffle()
-	card_database_reference = preload("res://CardDatabase.gd")
+	
 
 func draw_card():
 	
-	var card_drawn_name = player_deck[0]
-	player_deck.erase(card_drawn_name)
+	var card_drawn_name = player_deck.pop_front()
+	print(card_drawn_name)
 	#print("draw card")
 	
 	#if player drew the last card
 	if player_deck.size() ==0:
+		print("Deck is empty. No more cards to draw.")
 		$Area2D/CollisionShape2D.disabled = true
 		$Sprite2D.visible = false
-	
+		return
 	#var card_drawn_name = player_deck[0]
 	var card_scene = preload(CARD_SCENE_PATH)
 	
-	var card_image_path = str("res://assets/diamond_new/" + card_drawn_name + ".png")
+	var card_image_path = str("res://assets/Cards/" + card_drawn_name + ".png")
 	var new_card = card_scene.instantiate()
 	new_card.get_node("CardImage").texture = load(card_image_path)
 
