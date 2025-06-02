32bf744eab50b2e2bd0870757aa3c3bd910b1138
Yxa04 <isabellanicole0814@gmail.com>
2025-06-02 15:34:55 +0800

    Next Level Logic added
    Created GameManager.gd script and added node to Main.
    
    Added next level logic with the following functions:
    
    _ready() - starts the game manager
    start_lvl() - starts at 1st level, resets deck and hand, update UI, etc. (still updating)
    player_wins_lvl() - moves the player to the next lvl
    player_loses_lvl() - updates status of game (still playing -> game over)
    win_game() - ends game, stays in "congratulations" scene (still updating)
    Deck and Player hand can be resetted upon restart,
    
    Also, i fixed the issue regarding data path to rest player hand.

---
+++ GameManager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+
+
+var current_lvl := 1
+var current_round := 1
+var max_lvl := 9
+var is_game_over := false
+
+
+
+func _ready():
+	print("Game Manager Ready")
+	start_lvl()
+	
+	
+func start_lvl():
+	print("Starting lvl %d (Round %d)" % [current_lvl, current_round])
+	#_Get nodes from Deck and PlayerHand to reset
+	var deck = get_node("/root/Main/Deck")
+	var hand = get_node("/root/Main/PlayerHand")
+	deck.reset_deck()
+	hand.clear_player_hand()
+	#### Update UI, etc.
+	
+	
+#_Call this function if player wins
+func player_wins_lvl():
+	if current_lvl in [3, 6, 9]:
+		current_round += 1
+		
+	current_lvl += 1
+	
+	if current_lvl > max_lvl:
+		win_game()
+	else:
+		start_lvl()
+		
+		
+#_Call this function if player loses
+func player_loses_lvl():
+	is_game_over = true
+	print("Game Over! You lost.")
+	
+	
+#_Sends user to state to either go to main menu or restart
+func win_game():
+	print("Congratulations! You defeated all the bosses.")
